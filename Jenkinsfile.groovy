2176e98324eecd640ff689b000cb39ae6f5097b5
edilkk <edilmoldokanov@gmail.com>
2020-02-26 23:13:28 -0600

    unzip addedd

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,7 +7,7 @@ pipeline{
                 set +xe
                 echo Hello
                 ech Error
-                sudo yum install httpd wget -y
+                sudo yum install httpd wget unzip -y
                 ping -c 4 google.com
                 '''
             }
@@ -15,10 +15,50 @@ pipeline{
         stage("Download Terraform"){
             steps{
                 ws("tmp/"){
-                    sh "pwd"
-                    sh "wget https://releases.hashicorp.com/terraform/0.12.21/terraform_0.12.21_linux_amd64.zip"
+                    script{
+                        def exists = fileExists 'terraform_0.12.21_linux_amd64.zip'
+                        if(exists){
+                            sh "unzip terraform_0.12.21_linux_amd64.zip"
+                            sh "sudo mv terraform /bin"
+                            sh "terraform version"
+                        }else{
+                            sh "wget https://releases.hashicorp.com/terraform/0.12.21/terraform_0.12.21_linux_amd64.zip"
+                            sh "unzip terraform_0.12.21_linux_amd64.zip"
+                            sh "sudo mv terraform /bin"
+                            sh "terraform version"
+                        }
+                    }   
                 }
             }
+            
+        }
+        stage("Write to a file"){
+                steps{
+                    ws("tmp/"){
+                        writeFile text: "Test", file: "Testfile"
+                        sh "cat TestFile"
+                    }
+                }
+            }
+        stage("Download Packer"){
+            steps{
+                ws("tmp/"){
+                    script{
+                        def exists = fileExists 'terraform_0.12.21_linux_amd64.zip'
+                        if(exists){
+                            sh "unzip -o packer_1.5.4_linux_amd64.zip"
+                            sh "sudo mv packer /bin"
+                            sh "packer version"
+                        }else{
+                            sh "wget https://releases.hashicorp.com/packer/1.5.4/packer_1.5.4_linux_amd64.zip"
+                            sh "unzip -o packer_1.5.4_linux_amd64.zip"
+                            sh "sudo mv packer /bin"
+                            sh "packer version"
+                        }
+                    }   
+                }
+            }
+            
         }
     }
 }
